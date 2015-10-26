cc178d9379c47ec9b4780161a007fc85c956253e
scenarios <zyz0205@hotmail.com>
2015-10-26 15:27:46 +0800

    final version

--- SWE_individual_prime/main_prime.cpp
+++ SWE_individual_prime/main_prime.cpp
@@ -12,28 +12,28 @@ void main(void)
 	cout << "input index of prime" << endl;
 	cin >> index;
 	if (index < 1000000)
-		size = floor(1.6 * index);
+		size = floor(16 * index);
 	else if (1000000 <= index && index < 5000000)
-		size = floor(1.8 * index);
+		size = floor(18 * index);
 	else if (5000000 <= index && index < 10000000)
-		size = floor(1.8 * index);
+		size = floor(18 * index);
 	else if (10000000 <= index && index < 30000000)
-		size = floor(2 * index);
+		size = floor(20 * index);
 	else if (30000000 <= index && index < 50000000)
-		size = floor(2 * index);
+		size = floor(20 * index);
 	else if (50000000 <= index && index < 150000000)
-		size = floor(2.1 * index);
+		size = floor(21 * index);
 	else if (150000000 <= index && index < 200000000)
-		size = floor(2.2 * index);
+		size = floor(22 * index);
 	else
-		size = floor(2.5 * index);
+		size = floor(25 * index);
 
 
 	long long half = size / 2;
 	bool* p = new bool[half];
 	int sn = (int)sqrt(size);
 	for (long long i = 0; i < half; i++)
-		p[i] = true;// 初始化全部奇数为素数。p[0]对应3，即p[i]对应2*i+3   
+		p[i] = true;// 初始化全部奇数为素数。如p[0] = 3，即p[i] = 2*i+3   
 	for (long long i = 0; i < sn; i++) {
 		if (p[i]){
 			for (long long k = i + i + 3, j = k*i + k + i; j < half; j += k)
@@ -42,7 +42,7 @@ void main(void)
 
 	}
 
-	//	cout << p[i] << ' ';
+	
 	long long  count = 1;
 	for (long long i = 0; i < half; i++){
 		if (p[i])
@@ -52,8 +52,8 @@ void main(void)
 			break;
 		}
 	}
-	//	t2 = timeGetTime();
-	//	cout << endl << (t2 - t1)*1.0 / 1000 << endl;
+//		t2 = timeGetTime();
+//		cout << endl << (t2 - t1)*1.0 / 1000 << endl;
 
-	system("pause");
+	
 }
